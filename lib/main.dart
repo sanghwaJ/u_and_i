b5dac9d6ae1c353238158864502acdf3bd541c26
SanghwaJeong <jungsh0210@dbinc.co.kr>
2023-04-27 16:08:51 +0900

    fin

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,30 @@ import 'package:u_and_i/screen/home_screen.dart';
 void main() {
   runApp(
     MaterialApp(
+      // theme 관련은 main()에 지정하는 것이 일반적
+      theme: ThemeData(
+        fontFamily: 'sunflower', // 기본 폰트 지정
+        textTheme: TextTheme(
+          displayLarge: TextStyle(
+            color: Colors.white,
+            fontFamily: 'parisienne',
+            fontSize: 80.0,
+          ),
+          displayMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 50.0,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+          ),
+          bodySmall: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+          ),
+        ),
+      ),
       home: HomeScreen(),
     ),
   );
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -9,6 +9,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  DateTime now = DateTime(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  DateTime selectedDate = DateTime(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +34,11 @@ class _HomeScreenState extends State<HomeScreen> {
           width: MediaQuery.of(context).size.width,
           child: Column(
             children: [
-              _TopPart(),
+              _TopPart(
+                now: now,
+                selectedDate: selectedDate,
+                onPressed: onHeartPressed,
+              ),
               _BottomPart(),
             ],
           ),
@@ -30,21 +46,65 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+
+  void onHeartPressed(){
+      // dialog (팝업과 비슷..?)
+      showCupertinoDialog(
+        // iOS와 비슷한 디자인을 사용할 때 import
+        context: context,
+        // dialog 바깥을 클릭하면 dialog가 닫힘
+        barrierDismissible: true, // default는 false
+        // showCupertinoDialog는 builder가 있어야 widget을 만들 수 있음
+        builder: (BuildContext context) {
+          // Flutter의 특징 => 만일 특정 widget이 어디에 정렬해야할지 알 수 없으면 최대한의 사이즈를 차지함
+          // 그럴땐, 아래처럼 Align을 사용하면 됨
+          return Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              color: Colors.white,
+              height: 300.0,
+              // iOS DatePicker
+              child: CupertinoDatePicker(
+                mode: CupertinoDatePickerMode.date,
+                // 초기 DateTime 지정
+                initialDateTime: selectedDate,
+                // 최대 설정 가능한 DateTime 지정
+                maximumDate: DateTime(
+                  now.year,
+                  now.month,
+                  now.day,
+                ),
+                // 날짜나 시간이 바뀌었을 때 on 함수
+                onDateTimeChanged: (DateTime date) {
+                  // 변수를 변경할 때, build를 다시 실행할 땐 => setState
+                  setState(() {
+                    selectedDate = date;
+                  });
+                },
+              ),
+            ),
+          );
+        },
+      );
+  }
 }
 
 // private 선언
-class _TopPart extends StatefulWidget {
-  const _TopPart({Key? key}) : super(key: key);
+class _TopPart extends StatelessWidget {
+  final DateTime now;
+  final DateTime selectedDate;
+  final VoidCallback onPressed;
 
-  @override
-  State<_TopPart> createState() => _TopPartState();
-}
-
-class _TopPartState extends State<_TopPart> {
-  DateTime selectedDate = DateTime.now();
+  // 생성자
+  _TopPart({required this.now, required this.selectedDate, required this.onPressed, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // 위젯 트리 상 가장 가까운 theme을 가져옴
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+
     // Expanded => 현재 차지할 수 있는 최대한으로 크기 지정
     return Expanded(
       child: Column(
@@ -52,66 +112,23 @@ class _TopPartState extends State<_TopPart> {
         children: [
           Text(
             'U&I',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'parisienne',
-              fontSize: 80.0,
-            ),
+            style: textTheme.displayLarge,
           ),
           Column(
             children: [
               Text(
                 '우리 처음 만난 날',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'sunflower',
-                  fontSize: 30.0,
-                ),
+                style: textTheme.bodyMedium,
               ),
               Text(
-                '2020.01.01',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'sunflower',
-                  fontSize: 20.0,
-                ),
+                '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
+                style: textTheme.bodySmall,
               ),
             ],
           ),
           IconButton(
             iconSize: 60.0,
-            onPressed: () {
-              // dialog (팝업과 비슷..?)
-              showCupertinoDialog( // iOS와 비슷한 디자인을 사용할 때 import
-                context: context,
-                // dialog 바깥을 클릭하면 dialog가 닫힘
-                barrierDismissible: true, // default는 false
-                // showCupertinoDialog는 builder가 있어야 widget을 만들 수 있음
-                builder: (BuildContext context) {
-                  // Flutter의 특징 => 만일 특정 widget이 어디에 정렬해야할지 알 수 없으면 최대한의 사이즈를 차지함
-                  // 그럴땐, 아래처럼 Align을 사용하면 됨
-                  return Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Container(
-                      color: Colors.white,
-                      height: 300.0,
-                      // iOS DatePicker
-                      child: CupertinoDatePicker(
-                        mode: CupertinoDatePickerMode.date,
-                        // 날짜나 시간이 바뀌었을 때 on 함수
-                        onDateTimeChanged: (DateTime date){
-                          // 변수를 변경할 때, build를 다시 실행할 땐 => setState
-                          setState(() {
-                            selectedDate = date;
-
-                          });
-                        },
-                      ),
-                    ),
-                  );
-                },
-              );
-            },
+            onPressed: onPressed,
             icon: Icon(
               // 하트 아이콘
               Icons.favorite,
@@ -119,17 +136,21 @@ class _TopPartState extends State<_TopPart> {
             ),
           ),
           Text(
-            'D+1',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'sunflower',
-              fontSize: 50.0,
-              fontWeight: FontWeight.w700,
-            ),
+            'D ${returnDateCal(now, selectedDate)}',
+            style: textTheme.displayMedium,
           ),
         ],
       ),
     );
+  }
+
+  String returnDateCal(DateTime now, DateTime selectedDate) {
+    int dateCal = now.difference(selectedDate).inDays + 1;
+
+    if (dateCal < 0) {
+      return '- ${dateCal.abs().toString()}';
+    }
+    return '+ ${dateCal.toString()}';
   }
 }
 
